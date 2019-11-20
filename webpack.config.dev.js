e5d378a84b7174370e819d882ab05c9545784ed5
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-11-19 21:09:17 -0500

    Update template - Remove webpack - Update build/clean scripts - Add version/publishing scripts - Update tsconfig.json to publish type declarations

--- webpack.config.dev.js
+++
@@ -1,9 +0,0 @@
-const common = require("./webpack.config.common")
-const merge = require("webpack-merge")
-const webpack = require("webpack")
-
-module.exports = merge.smart(common, {
-  devtool: "cheap-module-eval-source-map",
-  mode: "development",
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-})
--- webpack.config.prod.js
+++
@@ -1,6 +0,0 @@
-const common = require("./webpack.config.common")
-const merge = require("webpack-merge")
-
-module.exports = merge.smart(common, {
-  mode: "production",
-})
